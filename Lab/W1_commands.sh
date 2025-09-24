6e9e3f908257d2c36ed546b43e15dc2a4bc82074
Khan.shizaalam <146518802+SheezaAlam@users.noreply.github.com>
2025-09-24 20:34:04 +0500

    Update W1_commands.sh

--- Lab/W1_commands.sh
+++ Lab/W1_commands.sh
@@ -79,6 +79,6 @@ pwd
 * Always double-check file paths and permissions when using commands that create or modify files.
 
 ---
-![image alt] (https://github.com/SheezaAlam/cybersecurity-ethicalhacking/blob/deac79e4688342b4097eb0a14681c75d0a972c32/ScreenShots/Screenshot%202025-09-24%20201324.png)
+![Screenshot 2025-09-24 201324](https://github.com/SheezaAlam/cybersecurity-ethicalhacking/blob/deac79e4688342b4097eb0a14681c75d0a972c32/ScreenShots/Screenshot%202025-09-24%20201324.png)
 
-![image alt] (https://github.com/SheezaAlam/cybersecurity-ethicalhacking/blob/deac79e4688342b4097eb0a14681c75d0a972c32/ScreenShots/Screenshot%202025-09-24%20201351.png)
+![Screenshot 2025-09-24 201351](https://github.com/SheezaAlam/cybersecurity-ethicalhacking/blob/deac79e4688342b4097eb0a14681c75d0a972c32/ScreenShots/Screenshot%202025-09-24%20201351.png)
