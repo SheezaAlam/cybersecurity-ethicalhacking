9b6f952681d386c0b8e88e390a0c3492ca0d83c9
Khan.shizaalam <146518802+SheezaAlam@users.noreply.github.com>
2025-10-12 05:08:25 -0500

    Update W3_sockets.py

--- Lab/Scripts/W3_sockets.py
+++ Lab/Scripts/W3_sockets.py
@@ -1,1 +1,16 @@
+┌──(shiza㉿kali)-[~]
+└─$ nc -nvlp 7777     
 
+#!bin/bash/python3
+#sockets
+import socket
+#sockets are used to make a connection between ports and ip add
+HOST = '127.0.0.1'
+PORT= 7777
+#stream used for establishing the connection
+s=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+#af_net is ipv4,strem is port
+s.connect((HOST,PORT))
+
+listening on [any] 7777 ...
+connect to [127.0.0.1] from (UNKNOWN) [127.0.0.1] 46552
